92e2ebe03d09de4ae75484e315248ca3a98b83d8
benli0822@gmail.com <benli0822@gmail.com@7fa6500e-aab1-f795-92bf-babe31d5f024>
2013-11-29 14:39:57 +0000

    finish of certificat and list all possibilies

--- 3sat/src/com/troissat/Formule.java
+++ 3sat/src/com/troissat/Formule.java
@@ -3,23 +3,29 @@ package com.troissat;
 import java.util.ArrayList;
 
 public class Formule {
-	
+
 	private ArrayList<Clause> listClause;
 
-	public Formule(ArrayList<Clause> listClause)
-	{
+	public Formule(ArrayList<Clause> listClause) {
 		this.listClause = listClause;
 	}
-	
-	public boolean calculerVrai(){
+
+	public Formule() {
+		this.listClause = new ArrayList<Clause>();
+	}
+
+	public void adjouteDansLeList(Clause c) {
+		this.listClause.add(c);
+	}
+
+	public boolean calculerVrai() {
 		int n = this.listClause.size();
-		boolean bool = false;
-		
-		for(int i=0; i<n; i++)
-		{
+		boolean bool = this.listClause.get(0).calculerVrai();
+
+		for (int i = 1; i < n; i++) {
 			bool = (this.listClause.get(i).calculerVrai()) && bool;
 		}
-		
+
 		return bool;
 	}
 }
---
+++ AlgoVerfication.java
@@ -0,0 +1,33 @@
+package com.troissat;
+
+import java.util.ArrayList;
+/**/
+/*(v[1] || !v[2] || v[4]) && (!v[1]  || v[3] || v[4]) && (!v[3] || !v[4] || v[2])*/
+public class AlgoVerfication {
+	public Formule TestFormule;
+	
+	public AlgoVerfication(Formule testFormule){
+		this.TestFormule = testFormule;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		Litteral v1 = new Litteral(1,true);
+		Litteral v2 = new Litteral(2,true);
+		Litteral v3 = new Litteral(3,true);
+		Litteral v4 = new Litteral(4,true);
+		
+		Clause c1 = new Clause(v1,v2,v4,false,true,false);
+		Clause c2 = new Clause(v1,v3,v4,true,false,false);
+		Clause c3 = new Clause(v3,v4,v2,true,true,false);
+
+		Formule f = new Formule();
+		f.adjouteDansLeList(c1);
+		f.adjouteDansLeList(c2);
+		f.adjouteDansLeList(c3);
+		
+		System.out.println(f.calculerVrai());
+	}
+}
