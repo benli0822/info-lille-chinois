70d893be145487ba7f01f7761351a9f22ad4bf35
jamescheng16@gmail.com <jamescheng16@gmail.com@7fa6500e-aab1-f795-92bf-babe31d5f024>
2013-12-21 08:50:11 +0000

    ajoute paser mais pas fini encore probeme avec les clasuse

---
+++ 3sat/src/com/troissat/paser.java
@@ -0,0 +1,43 @@
+package com.troissat;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class paser {
+
+	// an variable for all the data
+	public static byte[] liste_data;
+
+	paser() {
+
+	}
+
+	static // new the liste
+	void setSize(int size) {
+		System.out.println("data size:" + size);
+		liste_data = new byte[size];
+	}
+
+	static void readFile(String filename) throws IOException {
+		FileInputStream fin = new FileInputStream(filename);
+
+		// new the liste
+		setSize(fin.available());
+		// Read data into the array
+		fin.read(liste_data);
+		System.out.println((char)liste_data[6]);
+		for (int i = 0; i < liste_data.length; i++) {
+			System.out.print((char) liste_data[i]);
+		}
+		//System.out.println();
+		//System.out.println("data size:" + liste_data.length);
+		fin.close();
+
+	}
+
+	public static void main(String args[]) throws Exception {
+		String fname = "003.cnf";
+		readFile(fname);
+	}
+
+}
