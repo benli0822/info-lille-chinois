8e5b7137cb3b1406fd803b0fc28fafe3c0ef4594
jamescheng16@gmail.com <jamescheng16@gmail.com@7fa6500e-aab1-f795-92bf-babe31d5f024>
2014-01-19 19:55:08 +0000

    read txt file KMP algo

---
+++ MusicSearch/src/KMP.java
@@ -0,0 +1,100 @@
+import java.util.ArrayList;
+
+
+public class KMP {
+	  	private String text;  
+	    private String pattern;  
+	      
+	    KMP() {  
+	          
+	    }  
+	      
+	    KMP(String text, String pattern) {  
+	        this.text = text;  
+	        this.pattern = pattern;  
+	    }  
+	    
+	    KMP(ArrayList<String> motif, ArrayList<String> text){
+	    	this.text = "";
+	    	for(String line: text){
+	    		this.text += line;
+	    	}
+	    	this.pattern = "";
+	    	for(String pa : motif){
+	    		this.pattern += pa;
+	    	}
+	    	
+	    }
+	      
+	    public void setText(String text) {  
+	        this.text = text;  
+	    }  
+	      
+	    public void setPattern(String pattern) {  
+	        this.pattern = pattern;  
+	    }  
+	      
+	    public void KMPMatcher() {  
+	        int n = text.length();  
+	        int m = pattern.length();  
+	          
+	        int prefix[] = computePrefix();  
+	        int q = 0;  
+	          
+	        int count = 0;  
+	        for(int i = 0; i < n; i++) {  
+	              
+	            while(q > 0 && pattern.charAt(q)!= text.charAt(i)) {  
+	                q = prefix[q -1];  
+	            }  
+	              
+	            if(pattern.charAt(q) == text.charAt(i))  
+	                q++;  
+	              
+	            if(q == m) {  
+	                System.out.println("Pattern occurs with shift  " + ++count + "times");  
+	                q = prefix[q - 1];  
+	            }  
+	        }  
+	          
+	        if(count == 0) {  
+	            System.out.println("There is no matcher!");  
+	        }  
+	    }  
+	      
+	    private int[] computePrefix() {  
+	        int length = pattern.length();  
+	        int[] prefix = new int[length];  
+	          
+	        prefix[0] = 0;  
+	          
+	        int k = 0;  
+	        for(int i = 1; i < length; i++) {  
+	            while(k > 0 && pattern.charAt(k) != pattern.charAt(i)) {  
+	                k = prefix[k -1];   
+	            }  
+	            if(pattern.charAt(k) == pattern.charAt(i))  
+	                k++;  
+	            prefix[i] = k;  
+	        }  
+	          
+	        return prefix;  
+	    }  
+	      
+	  /*  public static void main(String[] args) {  
+	          
+	        KMP kmp;  
+	          
+	        if(args.length == 2) {  
+	            kmp = new KMP(args[0] , args[1]);  
+	        }  
+	        else {  
+	            kmp = new KMP();  
+	            kmp.setText("ababacabacbababababacabcbabababaca");  
+	            kmp.setPattern("ababaca");  
+	        }  
+	          
+	        kmp.KMPMatcher();  
+	          
+	    }  */
+}
---
+++ MusicSearch/src/SearchTest.java
@@ -0,0 +1,106 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+
+/**
+ * @author CHENG Xiaojun, JIN Benli et ZHAO Xuening
+ *
+ */
+public class SearchTest {
+	
+	// a list to save the motif
+	ArrayList<String> motif_list;
+	
+	//a list to save the orignal text
+	ArrayList<String> text_list;
+	
+	//a variable to decide wherther in debug mode 
+	boolean debug;
+	
+	public SearchTest(){
+		this.motif_list = new ArrayList<String>();
+		this.text_list = new ArrayList<String>();
+		
+		debug = true;
+		
+	}
+	public void ReadDataFromTxt() throws IOException {
+		// read motif.txt
+		BufferedReader br = new BufferedReader(new InputStreamReader(
+				getClass().getClassLoader().getResourceAsStream(
+			            "motif.txt")));
+		try {
+			StringBuilder sb = new StringBuilder();
+			String line = br.readLine();
+
+			while (line != null) {
+				sb.append(line);
+				sb.append('\n');
+				line = br.readLine();
+			}
+			String everything = sb.toString();
+			// Save all the words dans the list
+			//String[] resultat = everything.split("\\s|[\\s]|[^\\S]");
+			String[] resultat = everything.split(" ");
+			for (String s : resultat) {
+
+				this.motif_list.add(s);
+				// System.out.println(s);
+
+			}
+		} finally {
+			br.close();
+		}
+		// read text.txt
+		//br = new BufferedReader(new InputStreamReader(new FileInputStream(
+		//		"data/negative.txt"), "Latin1"));
+		br = new BufferedReader(new InputStreamReader(
+				getClass().getClassLoader().getResourceAsStream(
+			            "text.txt")));
+		try {
+			StringBuilder sb = new StringBuilder();
+			String line = br.readLine();
+
+			while (line != null) {
+				sb.append(line);
+				sb.append('\n');
+				line = br.readLine();
+			}
+			String everything = sb.toString();
+			// Save all the words in the list
+			String[] resultat = everything.split(" ");
+			for (String s : resultat) {
+				// System.out.println(s);
+				this.text_list.add(s);
+
+			}
+			if ( this.text_list.size() <= 2) {
+				System.out
+						.println("Read database error");
+			}
+		} finally {
+			br.close();
+		}
+
+	}
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		SearchTest test = new SearchTest();
+		test.ReadDataFromTxt();
+		
+		if(test.debug){
+		for(String s : test.text_list){
+			System.out.println(s);
+
+		}
+	
+		System.out.println("test total size: "+test.text_list.size());
+		}
+		
+		KMP kmp_test = new KMP(test.motif_list,test.text_list);
+		kmp_test.KMPMatcher();
+	}
+
+}
