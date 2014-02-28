028d719916753f074711863bcad6a69954ca2397
benli0822@gmail.com <benli0822@gmail.com@7fa6500e-aab1-f795-92bf-babe31d5f024>
2014-02-28 14:33:30 +0000

    可以找回文的类

---
+++ microARN/src/recherche/Palindrome.java
@@ -0,0 +1,136 @@
+package recherche;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author CHENG Xiaojun, JIN Benli et ZHAO Xuening
+ * Find all Palindrome in gemomics sequences
+ */
+public class Palindrome {
+
+	private String res; // Resource sequence
+	private ArrayList<String> lib; // Palindrome words
+	private int n; // Length of res
+	int memo[][]; // Matrix of Palindrome
+
+	public Palindrome(String res) {
+		this.res = res;
+		this.n = res.length();
+		memo = new int[n][n];
+		for (int i = 0; i < this.n; i++) {
+			for (int j = 0; j < this.n; j++) {
+				memo[i][j] = -1;
+			}
+		}
+		// all word is the Palindrome of itself
+		for (int i = 0; i < this.n; i++) {
+			memo[i][i] = 1;
+		}
+		find(0, this.n - 1);
+	}
+
+	/** 
+	 * Check for Palindrome for substring of resource
+	 * @param i
+	 * @param j
+	 * @return
+	 */
+	public boolean check(int i, int j) {
+		String toFind = res.substring(i, j + 1);
+		char[] g = toFind.toCharArray();
+		int length = g.length;
+		if (length == 2) {
+			if (g[0] != g[1]) {
+				return false;
+			}
+		} else {
+			for (int count = 0; count < length / 2; count++) {
+				if (g[count] != g[length - count - 1]) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+
+	/**
+	 * Iterator for checking the substring using dynamic programming with matrix of palindrome
+	 * @param i
+	 * @param j
+	 */
+	public void find(int i, int j) {
+		if (i <= j) {
+			if (memo[i][j] == -1)
+				if (check(i, j)) {
+					memo[i][j] = j - i + 1;
+				} else {
+					memo[i][j] = 0;
+				}
+			if (i + 1 < this.n && j - 1 >= 0) {
+				find(i + 1, j); // left tree
+				find(i, j - 1); // right tree
+			}
+		}
+	}
+
+	/**
+	 * Generate Palindrome words library from Palindrome matrix
+	 */
+	public void generateLib() {
+		this.lib = new ArrayList<String>();
+		for (int i = 0; i < this.n; i++) {
+			for (int j = 0; j < this.n; j++)
+				if (this.memo[i][j] > 0)
+					this.lib.add(res.substring(i, j + 1));
+		}
+	}
+
+	/**
+	 * Return the Palindrome words libray
+	 * @return
+	 */
+	public ArrayList<String> getLib() {
+		return lib;
+	}
+
+	/**
+	 * Print the Palindrome matrix
+	 * @param memo
+	 * @return
+	 */
+	public static String memoToString(int[][] memo) {
+		String result = "";
+		for (int i = 0; i < memo.length; ++i) {
+			for (int j = 0; j < memo[i].length; ++j) {
+				if (memo[i][j] == -1 || memo[i][j] >= 10)
+					result += memo[i][j] + " ";
+				else
+					result += memo[i][j] + "  ";
+			}
+			result += "\n";
+		}
+
+		return result;
+	}
+
+	/**
+	 * Get the Palindrome matrix
+	 * @return
+	 */
+	public int[][] getMemo() {
+		return memo;
+	}
+
+	public static void main(String[] args) {
+		Palindrome p = new Palindrome("arewenotdrawnonwardtonewera");
+		int[][] a = p.getMemo();
+		System.out.println(memoToString(a));
+		p.generateLib();
+		List<String> l = p.getLib();
+		for (String s : l) {
+			System.out.println(s);
+		}
+	}
+
+}
